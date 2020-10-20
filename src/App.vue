583f3f0d1f49f4836b5c8dffb663886de2bffe35
Mathew Teague <mathew.teague@textlocal.com>
2020-10-20 13:04:41 +0100

    learning life-cycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,29 @@
 <template>
   <div id="app">
     <Navbar />
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <h1>{{ title }}</h1>
-    <HelloWorld msg="Hello World! Welcome to Your Vue.js App"/>
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <HelloWorld msg="Hello World! Welcome to Your Vue.js App" />
+    <Blogs />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Navbar from './components/Navbar.vue'
-import AllFriends from './components/AllFriends.vue'
-import OnlineFriends from './components/OnlineFriends.vue'
+import HelloWorld from './components/HelloWorld'
+import Navbar from './components/Navbar'
+import Blogs from './components/Blogs'
 
 export default {
   name: 'App',
   data () {
     return {
-      title: 'Learning Vue',
-      friends: [
-        { name: 'Mario', online: true },
-        { name: 'Luigi', online: false },
-        { name: 'Toad', online: true },
-        { name: 'Bowser', online: false }
-      ]
+      title: 'Learning Vue'
     }
   },
   components: {
     HelloWorld,
     Navbar,
-    AllFriends,
-    OnlineFriends
-  },
-  methods: {
-    deleteFriend (payload) {
-      console.log(payload)
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
+    Blogs
   }
 }
 </script>
---
+++ src/components/Blogs.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="blogs">
+    <article class="blog">
+      <h2>{{ title }}</h2>
+      <button @click="changeTitle">Change Title</button>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Blogs',
+  data () {
+    return {
+      title: 'Blogs'
+    }
+  },
+  methods: {
+    changeTitle () {
+      this.title = 'Blogs all round'
+    }
+  },
+  beforeCreate () {
+    console.log('beforeCreate hook')
+  },
+  created () {
+    console.log('created hook')
+  },
+  beforeUpdate () {
+    alert('beforeUpdate hook')
+  }
+}
+</script>
+
+<style lang="scss"></style>
