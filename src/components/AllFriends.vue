7bf1bc2d8e8cf671d135981c4d6f72fab3f2a827
Mathew Teague <mathew.teague@textlocal.com>
2020-10-20 12:22:26 +0100

    added new files

---
+++ src/components/AllFriends.vue
@@ -0,0 +1,32 @@
+<template>
+  <section id="all-friends">
+    <h2>{{ title }}</h2>
+    <div class="friends" v-for="(friend, index) in friends" :key="index">
+      <span @click="unfriend(friend.name)">{{ friend.name }}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+// Can add object and type within props or have props as an array
+export default {
+  name: 'AllFriends',
+  props: {
+    friends: Array
+  },
+  data () {
+    return {
+      title: 'All Friends'
+    }
+  },
+  methods: {
+    unfriend (name) {
+      this.$emit('delete', { name })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
