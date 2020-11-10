6576a95c7846e571467e3fbbd00e8b6416739f4e
Mathew Teague <mathew.teague@textlocal.com>
2020-11-10 12:51:13 +0000

    added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <router-link to="/">Home</router-link>
+        <span v-if="index != Object.keys(items).length - 1"> | </span>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      items: [
+        { link: '/', text: 'Home' },
+        { link: '/about', text: 'About' }
+      ]
+    }
+  }
+}
+</script>
