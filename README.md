bbd2744d21bd535358e214a1013b70f7a3a44b4a
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-11-24 14:44:31 +0000

    added new project files

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ yarn serve
 yarn build
 ```
 
+### Run your end-to-end tests
+```
+yarn test:e2e
+```
+
 ### Lints and fixes files
 ```
 yarn lint
@@ -22,10 +27,3 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
-
-## Build Web Apps with Vue JS 2 & Firebase
-**By** The Net Ninja (Shaun Pelling)
-**Project structure** and setup by: Mathew Teague
-
-**Course Intro**
-Learn Vue JS & Firebase by creating & deploying dynamic web apps (including Authentication).
--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,12 @@
 <template>
   <div id="app">
-    <Navbar />
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>{{ title }}</h1>
-    <HelloWorld msg="Hello World! Welcome to Your Vue.js App"/>
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import Navbar from './components/Navbar.vue'
-import AllFriends from './components/AllFriends.vue'
-import OnlineFriends from './components/OnlineFriends.vue'
-
-export default {
-  name: 'App',
-  data () {
-    return {
-      title: 'Learning Vue',
-      friends: [
-        { name: 'Mario', online: true },
-        { name: 'Luigi', online: false },
-        { name: 'Toad', online: true },
-        { name: 'Bowser', online: false }
-      ]
-    }
-  },
-  components: {
-    HelloWorld,
-    Navbar,
-    AllFriends,
-    OnlineFriends
-  },
-  methods: {
-    deleteFriend (payload) {
-      console.log(payload)
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -52,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
